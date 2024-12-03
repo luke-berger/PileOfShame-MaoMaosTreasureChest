903e41cd29906d9fbf55af1f156dedfdb7b86046
Niclas Prapavat <niclas.prapavat@student.uni-luebeck.de>
2024-12-03 18:17:43 +0100

    eslint config die zweite

--- Pile_of_Shame_Website/MaoMaosTreasureChest/client/eslint.config.js
+++ Pile_of_Shame_Website/MaoMaosTreasureChest/client/eslint.config.js
@@ -1,12 +1,14 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+2 import pluginJs from "@eslint/js";
+3 import tseslint from "typescript-eslint";
+4 import eslintConfigPrettier from "eslint-config-prettier";
+5
+6
+7 /** @type {import('eslint').Linter.Config[]} */
+8 export default [
+9 {files: ["**/*.{js,mjs,cjs,ts}"]},
+10 {languageOptions: { globals: globals.browser }},
+11 pluginJs.configs.recommended,
+12 ...tseslint.configs.recommended,
+13 eslintConfigPrettier
+14 ];
