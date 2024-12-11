2c4ecba55a88cbe5c7622d8b4321c93ba623cd66
Luke Berger <luke.berger@student.uni-luebeck.de>
2024-12-11 21:40:54 +0100

    ts's in the git

---
+++ Pile_of_Shame_Website/MaoMaosTreasureChest/client/src/scripts/card.ts
@@ -0,0 +1,3 @@
+import { Item } from './item';
+import { itemExample } from './itemExample';
+
---
+++ Pile_of_Shame_Website/MaoMaosTreasureChest/client/src/scripts/item.ts
@@ -0,0 +1,14 @@
+export type Item = {
+  id:string, name:string, description:string, price:number, date:string, categories:string[], image:string
+};
+
+/*
+id: '1',
+name: 'Monopoly',
+description:
+  'Mal wieder eine Runde Monopoly mit den Freunden spielen. Ist schließlich ein lustiges Brettspiel für alle.',
+price: 29.99,
+date: '2022-11-11',
+categories: ['Brettspiel', 'Game'],
+image:
+*/
