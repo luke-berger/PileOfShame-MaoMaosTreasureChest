3b43868ce9e38657e90420cc462ff2a5f40a299f
Luke Berger <luke.berger@student.uni-luebeck.de>
2024-12-11 21:11:01 +0100

    task 3 done

---
+++ Pile_of_Shame_Website/MaoMaosTreasureChest/client/src/scripts/event-listener.ts
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Button und Input-Elemente finden
+    const button = document.getElementById('AddButton');
+    const inpElem1 = document.getElementById('input1') as HTMLInputElement;
+    const inpElem2 = document.getElementById('input2') as HTMLInputElement;
+    const inpElem3 = document.getElementById('input3') as HTMLInputElement;
+    const inpElem4 = document.getElementById('input4') as HTMLInputElement;
+    const fileInput = document.getElementById('inputImg') as HTMLInputElement;
+    const success = document.getElementById('success');
+    const fail = document.getElementById('fail');
+  
+    // Überprüfen, ob der Button existiert
+    if (!button) {
+      console.error('Button nicht gefunden!');
+      return;
+    }
+  
+    // Click-EventListener hinzufügen
+    button.addEventListener('click', () => {
+      // Werte der Eingabefelder trimmen und prüfen
+      const iptValue1 = inpElem1.value.trim();
+      const iptValue2 = inpElem2.value.trim();
+      const iptValue3 = inpElem3.value.trim();
+      const iptValue4 = inpElem4.value.trim();
+      const files = fileInput.files;
+  
+      // Eingabeprüfungen für jedes Feld
+      if (!iptValue1) {
+        inpElem1.classList.add('border-4', 'border-red-600');
+        fail?.classList.remove('hidden')
+      } else {
+        inpElem1.classList.remove('border-4', 'border-red-600');
+      }
+  
+      if (!iptValue2) {
+        inpElem2.classList.add('border-4', 'border-red-600');
+        fail?.classList.remove('hidden')
+      } else {
+        inpElem2.classList.remove('border-4', 'border-red-600');
+      }
+  
+      if (!iptValue3) {
+        inpElem3.classList.add('border-4', 'border-red-600');
+        fail?.classList.remove('hidden')
+      } else {
+        inpElem3.classList.remove('border-4', 'border-red-600');
+      }
+  
+      if (!iptValue4) {
+        inpElem4.classList.add('border-4', 'border-red-600');
+        fail?.classList.remove('hidden')
+      } else {
+        inpElem4.classList.remove('border-4', 'border-red-600');
+      }
+
+      if (!files || files.length === 0) {
+            // Kein Datei-Upload
+            fileInput.classList.add('border-4', 'border-red-600');
+            fail?.classList.remove('hidden')
+      } else {
+            // Datei-Upload vorhanden
+            fileInput.classList.remove('border-4', 'border-red-600');
+      }
+
+      if(iptValue1 && iptValue2 && iptValue3 && iptValue4 && files) {
+        success?.classList.remove('hidden');
+        fail?.classList.add('hidden');
+      } else {
+        success?.classList.add('hidden');
+      }
+
+    });
+  });
